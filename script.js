62b08350ed0dde45703cbe1c96d14226d93e0229
Hannah Odoi <hannahodoi45@gmail.com>
2025-04-13 19:20:06 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿// JavaScript to toggle visibility of project details
+document.addEventListener("DOMContentLoaded", function () {
+    const projectLinks = document.querySelectorAll('.project-link');
+
+    projectLinks.forEach(function (link) {
+        link.addEventListener('click', function (e) {
+            const details = e.target.nextElementSibling;
+            details.classList.toggle('show');
+        });
+    });
+});
+
+
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+    e.preventDefault(); // prevent page reload
+    alert("Message sent successfully!"); // simulate message send
+    this.reset(); // clear form
+});
